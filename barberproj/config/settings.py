ceed2363e78ec99458a23d1dd1ecf4653657e785
s7mihailomaric <mihailomaric001@gmail.com>
2023-10-17 11:49:16 +0200

    update sett and proc

--- barberproj/config/settings.py
+++ barberproj/config/settings.py
@@ -12,11 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9l0u5^@d%+3!ui+*(*l40_^4n^36c7@7t5tt1=a4ksehgl0s#='
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 load_dotenv()  # take environment variables from .env.
 
